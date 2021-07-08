3239e97c9294c4a78cd9ea51165eb66de5886a81
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-07-07 22:12:11 -0400

    07/07/2021

---
+++ Week 7/WS6/src/shapes/Shape.java
@@ -0,0 +1,15 @@
+/*
+Name: Davinder Verma
+Section: NBB
+Student Number: 121802201
+Email: dverma22@myseneca.ca
+Date: 08/06/2021
+Workshop: 2 (TASK-1)
+*/
+
+package shapes;
+
+@FunctionalInterface
+public interface Shape {
+    double perimeter(); //abstract method
+}
---
+++ Week 7/WS6/src/shapes/Square.java
@@ -0,0 +1,33 @@
+/*
+Name: Davinder Verma
+Section: NBB
+Student Number: 121802201
+Email: dverma22@myseneca.ca
+Date: 07/07/2021
+Workshop: 6
+*/
+
+package shapes;
+
+import java.text.DecimalFormat;
+
+public class Square extends Rectangle{
+
+    public Square(double s) throws Exception{
+        super(s, s);
+    }
+
+    private ShapeArea squareArea = () -> getW() * getW();
+
+    @Override
+    public double perimeter() {
+        return 4 * getW();
+    }
+
+    @Override
+    public String toString() {
+
+        DecimalFormat temp = new DecimalFormat("##0.0000##");
+        return String.format(this.getClass().getSimpleName() + " {s=" + getW() + "} perimeter = " + temp.format(perimeter()) + "\n" + "Area = " + temp.format(squareArea.area()) + "\n");
+    }
+}
